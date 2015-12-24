558a36b68afce9280fc056ed068a1e8368a45c06
samberic <sam@samatkinson.com>
2015-12-24 18:01:23 +1300

    Attempting an actual image

--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -7,18 +7,18 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
-            String port = Optional.ofNullable(System.getenv("PORT")).orElse("8080");
-            port(Integer.parseInt(port));
+        String port = Optional.ofNullable(System.getenv("PORT")).orElse("8080");
+        port(Integer.parseInt(port));
         get("/cage", "application/json", (req, res) -> {
-                res.type("application/json");
-                return "{\n" +
-                        "    \"text\": \"It's 80 degrees right now.\",\n" +
-                        "    \"attachments\": [\n" +
-                        "        {\n" +
-                        "            \"text\":\"Partly cloudy today and tomorrow\"\n" +
-                        "        }\n" +
-                        "    ]\n" +
-                        "}";
+            res.type("application/json");
+            return "{\n" +
+                    "    \"text\": \"Here is a calm image of Nic Cage.\",\n" +
+                    "    \"attachments\": [\n" +
+                    "        {\n" +
+                    "            \"image_url\": \"https://www.placecage.com/200/300\"," +
+                    "        }\n" +
+                    "    ]\n" +
+                    "}";
         });
     }
 }
