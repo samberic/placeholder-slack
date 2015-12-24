af391787dc8a0a4709c6b6a9dd2ddb83366ee082
samberic <sam@samatkinson.com>
2015-12-24 17:57:11 +1300

    misunderstood Spark API.  Modified to correctly return application type json

--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -1,18 +1,24 @@
 package com.samatkinson;
 
+import java.util.Optional;
+
 import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        port(Integer.parseInt(System.getenv("PORT")));
-        get("/cage", (req, res) -> "{\n" +
-                "    \"text\": \"It's 80 degrees right now.\",\n" +
-                "    \"attachments\": [\n" +
-                "        {\n" +
-                "            \"text\":\"Partly cloudy today and tomorrow\"\n" +
-                "        }\n" +
-                "    ]\n" +
-                "}");
+            String port = Optional.ofNullable(System.getenv("PORT")).orElse("8080");
+            port(Integer.parseInt(port));
+        get("/cage", "application/json", (req, res) -> {
+                res.type("application/json");
+                return "{\n" +
+                        "    \"text\": \"It's 80 degrees right now.\",\n" +
+                        "    \"attachments\": [\n" +
+                        "        {\n" +
+                        "            \"text\":\"Partly cloudy today and tomorrow\"\n" +
+                        "        }\n" +
+                        "    ]\n" +
+                        "}";
+        });
     }
 }
