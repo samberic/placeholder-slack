d41719db7663e685679cff0650f7d6230312864b
samberic <sam@samatkinson.com>
2015-12-24 17:48:41 +1300

    Using a simple attachment to make simpler for debugging. Specifying return type to JSON

--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -6,38 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         port(Integer.parseInt(System.getenv("PORT")));
-        get("/cage", (req, res) -> {
-            return "{" +
-                    "    \"attachments\": [" +
-                    "        {" +
-                    "            \"fallback\": \"Required plain-text summary of the attachment.\"," +
-                    "" +
-                    "            \"color\": \"#36a64f\"," +
-                    "" +
-                    "            \"pretext\": \"Optional text that appears above the attachment block\"," +
-                    "" +
-                    "            \"author_name\": \"Bobby Tables\"," +
-                    "            \"author_link\": \"http://flickr.com/bobby/\"," +
-                    "            \"author_icon\": \"http://flickr.com/icons/bobby.jpg\"," +
-                    "" +
-                    "            \"title\": \"Slack API Documentation\"," +
-                    "            \"title_link\": \"https://api.slack.com/\"," +
-                    "" +
-                    "            \"text\": \"Optional text that appears within the attachment\"," +
-                    "" +
-                    "            \"fields\": [" +
-                    "                {" +
-                    "                    \"title\": \"Priority\"," +
-                    "                    \"value\": \"High\"," +
-                    "                    \"short\": false" +
-                    "                }" +
-                    "            ]," +
-                    "" +
-                    "            \"image_url\": \"https://www.placecage.com/g/140/100\"," +
-                    "            \"thumb_url\": \"https://www.placecage.com/g/140/100\"" +
-                    "        }" +
-                    "    ]" +
-                    "}";
-        });
+        get("/cage", (req, res) -> "{\n" +
+                "    \"text\": \"It's 80 degrees right now.\",\n" +
+                "    \"attachments\": [\n" +
+                "        {\n" +
+                "            \"text\":\"Partly cloudy today and tomorrow\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}");
     }
 }
