f8ea7fb115ff651ce4f5dcefee8e5823e9ba29f4
samberic <sam@samatkinson.com>
2015-12-24 17:19:26 +1300

    adding port variable to work with heroku

--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -5,6 +5,7 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
+        port(Integer.parseInt(System.getenv(git "PORT")));
         get("/cage", (req, res) -> {
             return "{" +
                     "    \"attachments\": [" +
