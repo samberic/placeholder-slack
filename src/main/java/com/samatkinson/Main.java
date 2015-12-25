1f8f4de9dcfc2e98fdd78562185a10b68ae29825
samberic <sam@samatkinson.com>
2015-12-25 16:10:20 +1300

    trying mega message in case fields are mandatory

--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -11,12 +11,37 @@ public class Main {
         port(Integer.parseInt(port));
         get("/cage", "application/json", (req, res) -> {
             res.type("application/json");
-            return "{\n" +
-                    "    \"attachments\": [\n" +
-                    "        {\n" +
+            return "{" +
+                    "" +
+                    "    \"text\": \"Here is a calm image of Nic Cage.\"," +
+                    "    \"attachments\": [" +
+                    "        {" +
+                    "\"fallback\": \"Required plain-text summary of the attachment.\"," +
+                    "" +
+                    "            \"color\": \"#36a64f\"," +
+                    "" +
+                    "            \"pretext\": \"Optional text that appears above the attachment block\"," +
+                    "" +
+                    "            \"author_name\": \"Bobby Tables\"," +
+                    "            \"author_link\": \"http://flickr.com/bobby/\"," +
+                    "            \"author_icon\": \"http://flickr.com/icons/bobby.jpg\"," +
+                    "" +
+                    "            \"title\": \"Slack API Documentation\"," +
+                    "            \"title_link\": \"https://api.slack.com/\"," +
+                    "" +
+                    "            \"text\": \"Optional text that appears within the attachment\"," +
+                    "" +
+                    "            \"fields\": [" +
+                    "                {" +
+                    "                    \"title\": \"Priority\"," +
+                    "                    \"value\": \"High\"," +
+                    "                    \"short\": false" +
+                    "                }" +
+                    "            ]," +
                     "            \"image_url\": \"https://www.placecage.com/200/300.jpg\"" +
-                    "        }\n" +
-                    "    ]\n" +
+                    "            \"thumb_url\": \"http://example.com/path/to/thumb.png\""+
+                    "        }" +
+                    "    ]" +
                     "}";
         });
     }
