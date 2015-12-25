dbc65be991eb073ca90c8d76b3fb8435d3a682ed
samberic <sam@samatkinson.com>
2015-12-25 16:14:26 +1300

    reverting back, sigh

--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -16,30 +16,7 @@ public class Main {
                     "    \"text\": \"Here is a calm image of Nic Cage.\"," +
                     "    \"attachments\": [" +
                     "        {" +
-                    "\"fallback\": \"Required plain-text summary of the attachment.\"," +
-                    "" +
-                    "            \"color\": \"#36a64f\"," +
-                    "" +
-                    "            \"pretext\": \"Optional text that appears above the attachment block\"," +
-                    "" +
-                    "            \"author_name\": \"Bobby Tables\"," +
-                    "            \"author_link\": \"http://flickr.com/bobby/\"," +
-                    "            \"author_icon\": \"http://flickr.com/icons/bobby.jpg\"," +
-                    "" +
-                    "            \"title\": \"Slack API Documentation\"," +
-                    "            \"title_link\": \"https://api.slack.com/\"," +
-                    "" +
-                    "            \"text\": \"Optional text that appears within the attachment\"," +
-                    "" +
-                    "            \"fields\": [" +
-                    "                {" +
-                    "                    \"title\": \"Priority\"," +
-                    "                    \"value\": \"High\"," +
-                    "                    \"short\": false" +
-                    "                }" +
-                    "            ]," +
                     "            \"image_url\": \"https://www.placecage.com/200/300.jpg\"" +
-                    "            \"thumb_url\": \"http://example.com/path/to/thumb.png\""+
                     "        }" +
                     "    ]" +
                     "}";
