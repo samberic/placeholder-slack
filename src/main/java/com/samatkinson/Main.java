72372388b0c5649435994ff407cbe641cbf0b1f1
samberic <sam@samatkinson.com>
2015-12-26 11:01:20 +1300

    trying to cheat by using unfurling

--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -12,19 +12,7 @@ public class Main {
         get("/cage", "application/json", (req, res) -> {
             res.type("application/json");
             return "{" +
-                    "" +
-                    "    \"text\": \"Here is a calm image of Nic Cage.\"," +
-                    "    \"attachments\": [" +
-                    "        {" +
-                    "            \"fallback\": \"Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event\",\n" +
-                    "            \"title\": \"Network traffic (kb/s)\",\n" +
-                    "            \"title_link\": \"https://datadog.com/path/to/event\",\n" +
-                    "            \"text\": \"How does this look? @slack-ops - Sent by Julie Dodd\",\n" +
-                    "            \"image_url\": \"https://datadoghq.com/snapshot/path/to/snapshot.png\",\n" +
-                    "            \"color\": \"#764FA5\"" +
-
-                    "        }" +
-                    "    ]" +
+                    "    \"text\": \"<https://datadoghq.com/snapshot/path/to/snapshot.png>\"," +
                     "}";
         });
     }
