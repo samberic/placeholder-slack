28847e05f2b71a55e578df9bb6483fc4d80d2270
samberic <sam@samatkinson.com>
2015-12-26 11:19:53 +1300

    trying to remove some superfluous fields

--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -16,10 +16,6 @@ public class Main {
                     "    \"text\": \"Here is a calm image of Nic Cage.\"," +
                     "    \"attachments\": [" +
                     "        {" +
-                    "            \"fallback\": \"Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event\",\n" +
-                    "            \"title\": \"Network traffic (kb/s)\",\n" +
-                    "            \"title_link\": \"https://datadog.com/path/to/event\",\n" +
-                    "            \"text\": \"How does this look? @slack-ops - Sent by Julie Dodd\",\n" +
                     "            \"image_url\": \"https://www.placecage.com/200/300\",\n" +
                     "            \"color\": \"#764FA5\"" +
 
