81c4ccd67b7f17c097301dca77b611f343e701aa
samberic <sam@samatkinson.com>
2015-12-26 11:13:18 +1300

    that failed, going back to attachment with working image

--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -9,9 +9,23 @@ public class Main {
     public static void main(String[] args) {
         String port = Optional.ofNullable(System.getenv("PORT")).orElse("8080");
         port(Integer.parseInt(port));
-        get("/cage", "application/json", (req, res) -> {
+        get("/cage", (req, res) -> {
             res.type("application/json");
-            return "{\"text\": \"<http://imgs.xkcd.com/comics/regex_golf.png>\"}";
+            return "{" +
+                    "" +
+                    "    \"text\": \"Here is a calm image of Nic Cage.\"," +
+                    "    \"attachments\": [" +
+                    "        {" +
+                    "            \"fallback\": \"Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event\",\n" +
+                    "            \"title\": \"Network traffic (kb/s)\",\n" +
+                    "            \"title_link\": \"https://datadog.com/path/to/event\",\n" +
+                    "            \"text\": \"How does this look? @slack-ops - Sent by Julie Dodd\",\n" +
+                    "            \"image_url\": \"http://imgs.xkcd.com/comics/regex_golf.png\",\n" +
+                    "            \"color\": \"#764FA5\"" +
+
+                    "        }" +
+                    "    ]" +
+                    "}";
         });
     }
 }
