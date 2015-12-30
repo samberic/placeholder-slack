926eb2e9671af1d14b87b1bd915894a1022b20ac
samberic <sam@samatkinson.com>
2015-12-30 17:37:57 +1300

    little cleanup before go live

--- src/main/java/com/samatkinson/CageRequest.java
+++ src/main/java/com/samatkinson/CageRequest.java
@@ -4,23 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CageRequest {
-    static Map<String, String> commandMap = new HashMap<String, String>() {{
+    private final Map<String, String> commandMap = new HashMap<String, String>() {{
         put("calm", "");
         put("gray", "/g");
         put("crazy", "/c");
         put("gif", "/gif");
     }};
-    static String formatUrl = "https://www.placecage.com%s/%d/%d";
-    private HeightWidthGenerator heightWidthGenerator;
-    private String params;
+
+    private final String formatUrl = "https://www.placecage.com%s/%d/%d";
+    private final HeightWidthGenerator heightWidthGenerator;
+    private final String params;
 
     public CageRequest(HeightWidthGenerator heightWidthGenerator, String params) {
         this.heightWidthGenerator = heightWidthGenerator;
-        this.params = params;
+
         if (!commandMap.containsKey(params)) {
             System.out.println("Text did not match a mood. Text was: " + params);
-            this.params = "calm";
+            params = "calm";
         }
+
+        this.params = params;
+
     }
 
     public String getFormattedUrl() {
--- src/main/java/com/samatkinson/Main.java
+++ src/main/java/com/samatkinson/Main.java
@@ -3,6 +3,8 @@ package com.samatkinson;
 import java.util.Optional;
 import java.util.Random;
 
+import static java.lang.Integer.parseInt;
+import static java.util.Optional.*;
 import static spark.Spark.get;
 import static spark.Spark.port;
 
@@ -11,25 +13,14 @@ public class Main {
     private static CageRequest cageRequest;
 
     public static void main(String[] args) {
-        String port = Optional.ofNullable(System.getenv("PORT")).orElse("8080");
-        port(Integer.parseInt(port));
+
+        port(parseInt(ofNullable(System.getenv("PORT")).orElse("8080")));
+
         get("/cage", (req, res) -> {
             res.type("application/json");
             String params = req.queryParams("text");
 
-            cageRequest = new CageRequest(new HeightWidthGenerator() {
-                Random r = new Random();
-                @Override
-                public int height() {
-                    return 300 + r.nextInt(100);
-                }
-
-                @Override
-                public int width() {
-                    return 200 + r.nextInt(200);
-                }
-            }, params);
-            String formattedUrl = cageRequest.getFormattedUrl();
+            cageRequest = new CageRequest(new RandomSizeGenerator(), params);
 
             return "{" +
                     "\"response_type\": \"in_channel\"," +
@@ -37,7 +28,7 @@ public class Main {
                     "    \"attachments\": [" +
                     "        {" +
                     "            \"title\": \"Nic Cage\",\n" +
-                    "            \"image_url\": \"" + formattedUrl + "\",\n" +
+                    "            \"image_url\": \"" + cageRequest.getFormattedUrl() + "\",\n" +
                     "            \"color\": \"#764FA5\"" +
 
                     "        }" +
@@ -46,4 +37,17 @@ public class Main {
         });
     }
 
+    private static class RandomSizeGenerator implements HeightWidthGenerator {
+        Random r = new Random();
+
+        @Override
+        public int height() {
+            return 300 + r.nextInt(100);
+        }
+
+        @Override
+        public int width() {
+            return 200 + r.nextInt(200);
+        }
+    }
 }
